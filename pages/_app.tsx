ba0e89ba21ac0a908f59042bed459423d4ab9c1e
Greg Pfaff <greg.pfaff@razorfish.com>
2022-08-27 14:52:51 -0500

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,60 @@
-import React, { useState, ReactNode } from "react";
+import React, { useState, ReactNode, useEffect } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import GlobalStyles from "../styles/GlobalStyles";
-import type { AppLayoutProps } from "next/app";
+import type { AppLayoutProps, NextWebVitalsMetric } from "next/app";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric);
+}
 
 function App({ Component, pageProps }: AppLayoutProps) {
   const [queryClient] = useState(() => new QueryClient());
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-  const getLayout = Component.getLayout || ((page: ReactNode) => page)
+  const getLayout = Component.getLayout || ((page: ReactNode) => page);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <GlobalStyles />
-        {getLayout(<Component {...pageProps} />)};
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <GlobalStyles />
+          {getLayout(<Component {...pageProps} />)};
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   );
 }
 
