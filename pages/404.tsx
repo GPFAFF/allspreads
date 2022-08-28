d097b7d12300329fea43070fe15e31b01cf725c7
Greg Pfaff <greg.pfaff@razorfish.com>
2022-08-27 19:52:25 -0500

    fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,9 +3,22 @@ import React from "react";
 import Layout from "../components/layout";
 
 export default function Custom404() {
-  return <h2>404 - Page Not Found</h2>;
+  return (
+    <h2
+      style={{
+        display: "flex",
+        width: "100%",
+        marginTop: "50px",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      404 - Page Not Found
+    </h2>
+  );
 }
 
 Custom404.getLayout = function getLayout(page: NextComponentType) {
-  return <Layout>{page}</Layout>
+  return <Layout>{page}</Layout>;
 };
