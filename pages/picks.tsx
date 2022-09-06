4f8ce093ccf8cd48826d0c26d59e7033f6d50c16
Greg Pfaff <greg.pfaff@gmail.com>
2022-09-06 16:01:26 -0500

    add pick

--- pages/picks.tsx
+++ pages/picks.tsx
@@ -9,6 +9,7 @@ export default function Picks() {
       <h2>Picks</h2>
       <p>Record 1 - 0</p>
       <div className="winner">09/05/22 - Chicago White Sox -117</div>
+      <div>09/06/22 - Chicago Cubs / Cincinnati Reds U 8 -115</div>
     </div>
   );
 }
