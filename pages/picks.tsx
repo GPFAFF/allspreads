d86b191ca9441c7fc4edc6ab2acfecadf1ed54f2
Greg Pfaff <greg.pfaff@gmail.com>
2022-09-08 22:22:33 -0500

    update picks

--- pages/picks.tsx
+++ pages/picks.tsx
@@ -7,7 +7,7 @@ export default function Picks() {
   return (
     <div className="center">
       <h2>Picks</h2>
-      <p>Record 2 - 2 (50%)</p>
+      <p>Record 5 / 9 (55%)</p>
       <div className="winner">09/05/22 - Chicago White Sox -117</div>
       <div className="loser">
         09/06/22 - Chicago Cubs / Cincinnati Reds U 8 -115
@@ -16,11 +16,15 @@ export default function Picks() {
         09/06/22 - Chicago White Sox / Seattle Mariners O 7.5 -115
       </div>
       <div className="winner">09/07/22 - Philadelphia RL F5 -0.5</div>
-      <div>09/08/22 - Gabe Davis - Anytime TD</div>
-      <div>09/08/22 - Gabe Davis - Over 62.5 yards</div>
-      <div>09/08/22 - Josh Allen - Over 23.5 completions </div>
-      <div>09/08/22 - Cam Akers - Over 13.5 yards receiving </div>
-      <div>09/08/22 - Buffalo Bills / LA Rams O 52</div>
+      <div className="winner">09/08/22 - Gabe Davis - Anytime TD</div>
+      <div className="winner">09/08/22 - Gabe Davis - Over 62.5 yards</div>
+      <div className="winner">
+        09/08/22 - Josh Allen - Over 23.5 completions{" "}
+      </div>
+      <div className="loser">
+        09/08/22 - Cam Akers - Over 13.5 yards receiving{" "}
+      </div>
+      <div className="loser">09/08/22 - Buffalo Bills / LA Rams O 52</div>
     </div>
   );
 }
