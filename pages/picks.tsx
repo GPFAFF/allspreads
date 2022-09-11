d706237e1d027f55a35cbfe2aa80b9c535f27c72
Greg Pfaff <greg.pfaff@gmail.com>
2022-09-11 09:09:53 -0500

    add picks

--- pages/picks.tsx
+++ pages/picks.tsx
@@ -7,7 +7,7 @@ export default function Picks() {
   return (
     <div className="center">
       <h2>Picks</h2>
-      <p>Record 6 / 10 (60%)</p>
+      <p>Record 8 / 13 (61.5%)</p>
       <div className="winner">09/05/22 - Chicago White Sox -117</div>
       <div className="loser">
         09/06/22 - Chicago Cubs / Cincinnati Reds U 8 -115
@@ -26,6 +26,13 @@ export default function Picks() {
       </div>
       <div className="loser">09/08/22 - Buffalo Bills / LA Rams O 52</div>
       <div className="winner">09/09/22 - Louisville / UCF U 61.5</div>
+      <div className="loser">09/10/22 - Alabama -20 v Texas</div>
+      <div className="winner">09/10/22 - USC -8.5 v Stanford</div>
+      <div className="winner">09/10/22 - Tennessee -6.5 v Pittsburgh</div>
+      <div>09/11/22 - Baltimore 1H Under 22</div>
+      <div>09/11/22 - Nick Chubb Anytime TD</div>
+      <div>09/11/22 - Trey Lance Over 38.5 Rushing Yards</div>
+      <div>09/11/22 - Chargers / Raiders - O52</div>
     </div>
   );
 }
